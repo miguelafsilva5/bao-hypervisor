5c44526a5dc47b56de12816e5ca09b70c9269408
Miguel Silva <miguelafsilva5@gmail.com>
2025-03-26 11:59:23 +0000

    ref(arch_mpu): Add arch_mpu header
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

---
+++ src/arch/armv8/armv8-r/inc/arch/mpu.h
@@ -0,0 +1,19 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+#ifndef __ARCH_MPU_H__
+#define __ARCH_MPU_H__
+
+#include <bao.h>
+#include <arch/sysregs.h>
+
+void mpu_arch_init(void);
+void mpu_arch_enable(void);
+bool mpu_add_region(struct mp_region* reg, bool locked);
+bool mpu_remove_region(struct mp_region* reg);
+bool mpu_update_region(struct mp_region* reg);
+bool mpu_perms_compatible(unsigned long perms1, unsigned long perms2);
+
+#endif /* __ARCH_MPU_H__ */
