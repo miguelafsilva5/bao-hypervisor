f48f1fc6d5192999f555e8eb6cbadd264f4877f6
Miguel Silva <miguelafsilva5@gmail.com>
2025-04-29 15:22:54 +0100

    ref(armv8-r): Update armv8-r MPU driver
    The MPU driver for armv8-r had to be updated according to the new VMPU
    implementation.
    
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

--- src/arch/armv8/armv8-r/inc/arch/mpu.h
+++ src/arch/armv8/armv8-r/inc/arch/mpu.h
@@ -8,12 +8,21 @@
 
 #include <bao.h>
 #include <arch/sysregs.h>
+#include <bitmap.h>
+#include <mem.h>
 
-void mpu_arch_init(void);
-void mpu_arch_enable(void);
-bool mpu_add_region(struct mp_region* reg, bool locked);
-bool mpu_remove_region(struct mp_region* reg);
-bool mpu_update_region(struct mp_region* reg);
+struct mpu_perms {
+    perms_t el2;
+    perms_t el1;
+};
+
+struct mpu_arch {
+    unsigned long bitmap;
+    unsigned long locked;
+
+    struct mpu_perms entry_perms[MPU_ARCH_MAX_NUM_ENTRIES];
+};
+
 bool mpu_perms_compatible(unsigned long perms1, unsigned long perms2);
 
 #endif /* __ARCH_MPU_H__ */
